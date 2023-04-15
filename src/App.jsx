42d207ccc382096c9bd80c67528a812fe1526812
Karina Boronina <Gubashina@yandex.ru>
2023-04-15 12:56:37 +0300

    Создание ререндиринга и зависимостей в случае наличия user

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import testData from './assents/data.json'; //Массив с данными карточек
 import Card from './components/Card/Card'; //Компонент обертка для карточек
 // import Header from './components/General/index'; //Можно и таким образом подключить './components/Header'
@@ -7,9 +8,12 @@ import Promo from './components/Promo/Promo';
 const promoData = ['=)', '^_^', 'O_o', 'X_x', ';=('];
 
 const App = () => {
+    // const user = localStorage.getItem('user12');
+    const [user, setUser] = useState(localStorage.getItem('user12'));
+
     return (
         <>
-            <Header />
+            <Header user={user} upd={setUser} />
             <div className="container">
                 {testData.map((el, index) => {
                     return (
