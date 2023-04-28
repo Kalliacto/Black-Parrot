b8be9cfe9ac37944ed9afa7bb7297949e0ff390b
Kalliacto <98468178+Kalliacto@users.noreply.github.com>
2023-04-28 12:04:24 +0300

    страница избранного

--- src/components/Header/HeaderIcons/HeaderIcons.jsx
+++ src/components/Header/HeaderIcons/HeaderIcons.jsx
@@ -9,8 +9,9 @@ const HeaderIcons = (props) => {
     return (
         <div className="header__icons">
             <div>
-                <Link to={'/favorite'}>
+                <Link className="header__heart" to={'/favorite'}>
                     <Heart />
+                    <span className="header__icons_bubble">11</span>
                 </Link>
             </div>
             <div>
--- src/pages/FavoritePage/FavoritePage.jsx
+++ src/pages/FavoritePage/FavoritePage.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './favoritePage.css';
 import GoBack from '../../components/GoBack/GoBack';
+import { CardList } from '../../components/CardList/CardList';
 
-const FavoritePage = (props) => {
+const FavoritePage = ({ favorite, userId, changeLikeCard }) => {
     return (
         <div className="favorite__wrapper">
             <GoBack />
             <h2 className="favorite__title">Избранное</h2>
             <div></div>
-            {/* CardList */}
+            <CardList
+                cards={favorite}
+                changeLikeCard={changeLikeCard}
+                userId={userId}
+            />
         </div>
     );
 };
