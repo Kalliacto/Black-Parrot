67e2a60c8e9b191c5fcb3c9154765b8281e420c5
Karina Boronina <Gubashina@yandex.ru>
2023-04-30 19:39:54 +0300

    чутка почистила лишнее

--- src/components/ProductView/ProductView.jsx
+++ src/components/ProductView/ProductView.jsx
@@ -1,70 +1,85 @@
-// import React, { useContext } from 'react';
-// import './productView.css';
-// import GoBack from '../GoBack/GoBack';
-// import { ReactComponent as Like } from '../Card/img/Like.svg';
-// import { CardContext } from '../../context/cardContext';
+import React, { useContext } from 'react';
+import './productView.css';
+import GoBack from '../GoBack/GoBack';
+import { ReactComponent as Like } from '../Card/img/Like.svg';
+import { CardContext } from '../../context/cardContext';
+import { editLikeCard } from '../../utils/api';
 
-// const ProductView = ({ productInfo }) => {
-//     const { user, changeLikeCard } = useContext(CardContext);
+const ProductView = ({ productInfo, setProductInfo, id }) => {
+    const { card, user, setCards, findFavorite, setFavorite, localStorage } =
+        useContext(CardContext);
 
-//     // const foo = async (productInfo, user) => {
-//     //     const cardLiked = await productInfo.likes.some(
-//     //         (item) => item === user._id
-//     //     );
-//     //     changeLikeCard(productInfo._id, cardLiked);
-//     // };
+    const cardIsLiked = productInfo.likes
+        ? productInfo.likes.includes(user._id)
+        : false;
 
-//     return (
-//         <>
-//             <div className="product__wrapper">
-//                 <div className="product__title_wrapper">
-//                     <GoBack />
-//                     <h3 className="product__title">{productInfo.name}</h3>
-//                     <div className="product__rating">
-//                         <span>Artikul</span>
-//                         <span>Rate</span>
-//                     </div>
-//                 </div>
-//                 <div className="product__img_wrapper">
-//                     <div className="card__sticky card__sticky_left">
-//                         {productInfo.discount ? (
-//                             <span className="card__discount">
-//                                 -{productInfo.discount}%
-//                             </span>
-//                         ) : (
-//                             ''
-//                         )}
-//                     </div>
-//                     <div className="card__sticky card__sticky_right">
-//                         <button
-//                             // onClick={() => foo(productInfo, user)}
-//                             className={`btn__like ${
-//                                 productInfo.likes
-//                                     ? 'card__like_active'
-//                                     : 'card__like'
-//                             }`}
-//                         >
-//                             <Like />
-//                         </button>
-//                     </div>
-//                     <img
-//                         className="product__img"
-//                         src={productInfo.pictures}
-//                         alt=""
-//                     />
-//                 </div>
-//                 <div className="product__description">
-//                     <span className="product__description_price">
-//                         {productInfo.price}&nbsp;₽
-//                     </span>
-//                 </div>
-//                 <div className="product__description">
-//                     <span className="product__description_title">Описание</span>
-//                     <span>{productInfo.description}</span>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// };
+    const changeLikeCardOne = async (id, cardIsLiked) => {
+        const updateLikeInCard = await editLikeCard(id, cardIsLiked).catch(
+            (error) => console.log(error)
+        );
 
-// export default ProductView;
+        const newCard = card.map((item) =>
+            item._id === updateLikeInCard._id ? updateLikeInCard : item
+        );
+        setProductInfo(updateLikeInCard);
+        setCards([...newCard]);
+        localStorage.setItem('card', JSON.stringify(newCard));
+
+        const newFavorite = newCard.filter((item) =>
+            findFavorite(item, user._id)
+        );
+        setFavorite(newFavorite);
+    };
+
+    return (
+        <>
+            <div className="product__wrapper">
+                <div className="product__title_wrapper">
+                    <GoBack />
+                    <h3 className="product__title">{productInfo.name}</h3>
+                    <div className="product__rating">
+                        <span>Artikul</span>
+                        <span>Rate</span>
+                    </div>
+                </div>
+                <div className="product__img_wrapper">
+                    <div className="card__sticky card__sticky_left">
+                        {productInfo.discount ? (
+                            <span className="card__discount">
+                                -{productInfo.discount}%
+                            </span>
+                        ) : (
+                            ''
+                        )}
+                    </div>
+                    <div className="card__sticky card__sticky_right">
+                        <button
+                            onClick={() => changeLikeCardOne(id, cardIsLiked)}
+                            className={`btn__like ${
+                                cardIsLiked ? 'card__like_active' : 'card__like'
+                            }`}
+                        >
+                            <Like />
+                        </button>
+                    </div>
+                    <img
+                        className="product__img"
+                        src={productInfo.pictures}
+                        alt=""
+                    />
+                </div>
+                <div className="product__description">
+                    <span className="product__description_price">
+                        {productInfo.price}&nbsp;₽
+                    </span>
+                </div>
+                <div className="product__description">
+                    <span className="product__description_title">Описание</span>
+                    <span>{productInfo.description}</span>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ProductView;
