75e80a22bec2b530829631148274ca7ccab6b338
Karina Boronina <Gubashina@yandex.ru>
2023-05-08 17:13:30 +0300

    Создан акордеон в FAQ

--- src/pages/FAQ/FAQ.jsx
+++ src/pages/FAQ/FAQ.jsx
@@ -1,32 +1,42 @@
 import React, { useState } from 'react';
 import './faq.css';
+import data from '../../data/popapData.json';
 
 const FAQ = () => {
-    const [activeState, setActiveState] = useState(false);
+    const [selected, setSelected] = useState(null);
+    const toggle = (index) => {
+        if (selected === index) {
+            return setSelected(null);
+        }
+        return setSelected(index);
+    };
 
     return (
         <div className="faq__wrapper">
             <h2 className="faq__title">Часто спрашивают</h2>
-            <div className="faq__content">
-                <div className="faq__subtitle">
-                    <span className="faq__span">{activeState ? '-' : '+'}</span>
-                    <p onClick={() => setActiveState(!activeState)}>
-                        Случайный текст похож на гитарный перебор?
-                    </p>
-                </div>
-                {activeState ? (
-                    <span className="faq__info">
-                        Прежде всего, дальнейшее развитие различных форм
-                        деятельности предоставляет широкие возможности для
-                        дальнейших направлений развития. В своём стремлении
-                        повысить качество жизни, они забывают, что семантический
-                        разбор внешних противодействий представляет собой
-                        интересный эксперимент проверки инновационных методов
-                        управления процессами.
-                    </span>
-                ) : (
-                    ''
-                )}
+            <div className="faq__accordion">
+                {data.map((item, index) => {
+                    return (
+                        <div className="faq__accordion_item" key={index}>
+                            <div
+                                className="faq__accordion_title"
+                                onClick={() => toggle(index)}
+                            >
+                                <span>{selected === index ? '-' : '+'}</span>
+                                <h3>{item.title}</h3>
+                            </div>
+                            <div
+                                className={
+                                    selected === index
+                                        ? 'faq__accordion_content show'
+                                        : 'faq__accordion_content'
+                                }
+                            >
+                                {item.text}
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
