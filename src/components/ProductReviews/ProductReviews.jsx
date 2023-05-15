1c1021081da8677f13b26ff511c228e7d22cd18e
Karina Boronina <Gubashina@yandex.ru>
2023-05-15 11:28:18 +0300

    добавление отзыва с отрисовкой его сразу

--- src/components/ProductReviews/ProductReviews.jsx
+++ src/components/ProductReviews/ProductReviews.jsx
@@ -1,22 +1,31 @@
-import React, { useState } from 'react';
+import React, { memo, useContext, useEffect, useState } from 'react';
 import './productReviews.css';
 import { timeOptions } from '../../utils/utils';
 import Rate from '../Rate/Rate';
 import { useForm } from 'react-hook-form';
 import { api } from '../../utils/api';
+import { CardContext } from '../../context/cardContext';
+import { Trash3} from 'react-bootstrap-icons'
 
-const ProductReviews = ({ productInfo }) => {
+const ProductReviews = memo(({ productInfo }) => {
     const [formActive, setFormActive] = useState(false);
     const { register, handleSubmit, reset } = useForm({});
+    const [allReviews, setAllReviews] = useState([]);
+    const { user } = useContext(CardContext);
+
+    console.log(user._id);
+    useEffect(() => {
+        api.getProductAllReviews(productInfo._id).then((data) => setAllReviews(data));
+    }, []);
 
     const submitReview = async (review) => {
         return await api
             .addNewReview(productInfo._id, review)
+            .then((review) => setAllReviews((state) => [review, ...state]))
             .then(reset())
             .catch((error) => console.log(error));
     };
 
-    console.log({ productInfo });
     return (
         <div className='product__reviews'>
             <h2 className='product__reviews_title'>Отзывы</h2>
@@ -33,13 +42,19 @@ const ProductReviews = ({ productInfo }) => {
                         className='form__reviews_input'
                         rows={3}
                     />
+                    <input
+                        type='number'
+                        {...register('rating')}
+                        placeholder='Ваша оценка от 0 до 5'
+                        className='form__reviews_input'
+                    />
                     <button type='submit' className='reviews_btn'>
                         Отправить
                     </button>
                 </form>
             )}
             <div className='reviews__list'>
-                {productInfo.reviews.map((item) => {
+                {allReviews.map((item) => {
                     return (
                         <div key={item._id} className='reviews__item'>
                             <div className='reviews__name-wrap'>
@@ -59,6 +74,6 @@ const ProductReviews = ({ productInfo }) => {
             </div>
         </div>
     );
-};
+});
 
 export default ProductReviews;
